97fbda09861790880adb6aedfcaf2db5c6f300fe
oyuk <okysoft68@gmail.com>
2016-10-16 12:10:10 +0900

    [from now] 2016/10/16 12:10:10
     .gitignore                                         |  54 +++++++
     .idea/compiler.xml                                 |  22 +++
     .idea/copyright/profiles_settings.xml              |   3 +
     .idea/encodings.xml                                |   6 +
     .idea/misc.xml                                     |  46 ++++++
     .idea/modules.xml                                  |   9 ++
     .idea/runConfigurations.xml                        |  12 ++
     .idea/vcs.xml                                      |   6 +
     app/.gitignore                                     |   1 +
     app/build.gradle                                   |  37 +++++
     app/proguard-rules.pro                             |  17 +++
     .../kotlinyaruzo/ExampleInstrumentedTest.java      |  26 ++++
     app/src/main/AndroidManifest.xml                   |  20 +++
     .../com/okysoft/kotlinyaruzo/MainActivity.kt       |  13 ++
     app/src/main/res/layout/activity_main.xml          |  17 +++
     app/src/main/res/mipmap-hdpi/ic_launcher.png       | Bin 0 -> 3418 bytes
     app/src/main/res/mipmap-mdpi/ic_launcher.png       | Bin 0 -> 2206 bytes
     app/src/main/res/mipmap-xhdpi/ic_launcher.png      | Bin 0 -> 4842 bytes
     app/src/main/res/mipmap-xxhdpi/ic_launcher.png     | Bin 0 -> 7718 bytes
     app/src/main/res/mipmap-xxxhdpi/ic_launcher.png    | Bin 0 -> 10486 bytes
     app/src/main/res/values-w820dp/dimens.xml          |   6 +
     app/src/main/res/values/colors.xml                 |   6 +
     app/src/main/res/values/dimens.xml                 |   5 +
     app/src/main/res/values/strings.xml                |   3 +
     app/src/main/res/values/styles.xml                 |  11 ++
     .../com/okysoft/kotlinyaruzo/ExampleUnitTest.java  |  17 +++
     build.gradle                                       |  25 ++++
     gradle.properties                                  |  17 +++
     gradle/wrapper/gradle-wrapper.jar                  | Bin 0 -> 53636 bytes
     gradle/wrapper/gradle-wrapper.properties           |   6 +
     gradlew                                            | 160 +++++++++++++++++++++
     gradlew.bat                                        |  90 ++++++++++++
     settings.gradle                                    |   1 +
     33 files changed, 636 insertions(+)

---
+++ app/src/main/kotlin/com/okysoft/kotlinyaruzo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.okysoft.kotlinyaruzo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity:AppCompatActivity() {
+
+    protected override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
